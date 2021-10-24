f67a5e624c810ffae7451586bef040781344d783
jerem <jerem@LAPTOP>
2021-10-24 17:15:11 +0200

    graphic rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
+import batman from './tokens/batman.png';
+import superman from './tokens/superman.png';
+import aquaman from './tokens/aquaman.png';
+import wonderWoman from './tokens/wonder-woman.png';
+import flash from './tokens/flash.png';
+import cyborg from './tokens/cyborg.png';
 
 function App() {
 
   const width = 8;
-  const characterTokens =['batman', 'superman', 'aquaman', 'wonder-woman', 'flash', 'cyborg'];
+  const characterTokens =[batman, superman, aquaman, wonderWoman, flash, cyborg];
   const[currentTokenDisplay, setCurrentTokenDisplay] =useState([]);
 
   const boardGame = () => {
@@ -21,7 +27,13 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div className="game-board">
+        {currentTokenDisplay.map((characterToken, index) => (
+          <div className="case">
+            <img key={index} src={characterToken} alt={characterToken}/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

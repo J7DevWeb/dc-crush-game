e9e288331c885cf3766b425d3c385f7dd51ce689
jerem <jerem@LAPTOP>
2021-10-24 19:16:34 +0200

    checking rows

--- src/App.js
+++ src/App.js
@@ -23,6 +23,20 @@ function App() {
     }
   };
 
+  const checkRowOfFour = () => {
+    for (let i = 0; i < 64; i++){
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const choosenColor = currentTokenDisplay[i];
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64];
+
+      if (notValid.includes(i)) continue
+
+      if (rowOfFour.every(boardCase => currentTokenDisplay[boardCase] === choosenColor)){
+        rowOfFour.forEach(boardCase => currentTokenDisplay[boardCase] = '');
+      }
+    }
+  };
+
   const checkColumnOfThree = () => {
     for (let i = 0; i < 47; i++){
       const columnOfThree = [i, i + width, i + width * 2];
@@ -30,6 +44,20 @@ function App() {
 
       if (columnOfThree.every(boardCase => currentTokenDisplay[boardCase] === choosenColor)){
         columnOfThree.forEach(boardCase => currentTokenDisplay[boardCase] = '');
+      }
+    }
+  };
+
+  const checkRowOfThree = () => {
+    for (let i = 0; i < 64; i++){
+      const rowOfThree = [i, i + 1, i + 2];
+      const choosenColor = currentTokenDisplay[i];
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+
+      if (notValid.includes(i)) continue
+
+      if (rowOfThree.every(boardCase => currentTokenDisplay[boardCase] === choosenColor)){
+        rowOfThree.forEach(boardCase => currentTokenDisplay[boardCase] = '');
       }
     }
   };
@@ -50,11 +78,13 @@ function App() {
   useEffect(() => {
     const timer = setInterval(() => {
       checkColumnOfFour();
+      checkRowOfFour();
       checkColumnOfThree();
+      checkRowOfThree();
       setCurrentTokenDisplay([...currentTokenDisplay]);
     },100);
     return () => clearInterval(timer);
-  }, [checkColumnOfFour, checkColumnOfThree, currentTokenDisplay]);
+  }, [checkColumnOfFour, checkRowOfFour, checkColumnOfThree, checkRowOfThree, currentTokenDisplay]);
 
   return (
     <div className="App">

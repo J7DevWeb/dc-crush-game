1674ecd6724f4e6827b3251209acf110fce0155c
jerem <jerem@LAPTOP>
2021-10-24 16:34:40 +0200

    logical part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const width = 8;
+  const characterTokens =['batman', 'superman', 'aquaman', 'wonder-woman', 'flash', 'cyborg'];
+  const[currentTokenDisplay, setCurrentTokenDisplay] =useState([]);
+
+  const boardGame = () => {
+    const randomTokenDisplay = [];
+    for (let i = 0; i < width * width; i++){
+      const randomToken = characterTokens[Math.floor(Math.random() * characterTokens.length)];
+      randomTokenDisplay.push(randomToken);
+    }
+    setCurrentTokenDisplay(randomTokenDisplay);
+  };
+
+  useEffect(() => {
+    boardGame();
+  }, []);
+
   return (
     <div className="App">
       

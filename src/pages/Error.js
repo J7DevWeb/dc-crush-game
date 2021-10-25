4070efb07c49f79716a3eccb7932eaf93b1ca72b
jerem <jerem@LAPTOP>
2021-10-25 02:13:20 +0200

    score board and visual improvment

---
+++ src/pages/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Error = () => {
+    return (
+        <div className="error">
+            <Navigation />
+            <h1>Cette page n'existe pas !!!</h1>
+            <img src="logos/favicon.png" />
+        </div>
+    );
+};
+
+export default Error;
